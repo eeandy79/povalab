c66bdb96132d98d1646d7047d76233ea0fec6110
Ubuntu <ubuntu@ip-172-31-12-253.ap-east-1.compute.internal>
2021-08-18 07:29:22 +0000

    add seo to index-page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
@@ -16,6 +17,13 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
+    <Helmet>
+      <title>{title}</title>
+      <meta
+        name="description"
+        content={description}
+      />
+    </Helmet>
     <div
       className="full-width-image margin-top-0"
       style={{
